8cc31a752f4d49df8a4318712276284e3748a87a
Carlos Eduardo <carloseduardomveloso@gmail.com>
2025-06-20 13:47:18 -0300

    adicionando a funcionalidade de filtro

--- scripts/negocios.js
+++ scripts/negocios.js
@@ -1,8 +1,22 @@
-const btnFiltrar = document.getElementById('pesquisar');
+const btnFiltrar = document.getElementById("pesquisar");
 
-btnFiltrar.addEventListener('click', filtrar)
+btnFiltrar.addEventListener("click", filtrar);
 
 function filtrar() {
-    console.log('Teste');
-    
+  const categoriaSelecionada = document.getElementById("categorias").value;
+  const clientes = document.querySelectorAll(".card");
+
+  clientes.forEach(function (cliente) {
+    const categoriaCliente = cliente.dataset.categoria;
+
+    const mostrarCliente =
+      categoriaSelecionada === "" ||
+      categoriaCliente === categoriaSelecionada;
+
+    if (mostrarCliente) {
+      cliente.style.display = "flex"; // Mostra os que passaram no filtro
+    } else {
+      cliente.style.display = "none"; // Esconde os que não passaram
+    }
+  });
 }
